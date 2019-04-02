87e4346f3264db11d186d8eee27b9d467fbfe34f
Sergeev Kirill <Sergeev.Kirill.S@yandex.ru>
2019-04-03 03:16:57 +0400

    add permission telephone check

--- samples/src/main/java/com/framgia/example/emulatordetector/MainActivity.java
+++ samples/src/main/java/com/framgia/example/emulatordetector/MainActivity.java
@@ -37,8 +37,8 @@ public class MainActivity extends AppCompatActivity {
         // NoxPlayer
         // KoPlayer
         // MEmu
-
-        MainActivityPermissionsDispatcher.checkEmulatorDetectorWithCheck(this);
+        checkEmulatorDetector();
+        //MainActivityPermissionsDispatcher.checkEmulatorDetectorWithCheck(this);
     }
 
     @NeedsPermission(Manifest.permission.READ_PHONE_STATE)
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void checkWith(boolean telephony) {
         EmulatorDetector.with(this)
-            .setCheckTelephony(telephony)
+            .setCheckTelephony(telephony,true)
             .addPackageName("com.bluestacks")
             .setDebug(true)
             .detect(new EmulatorDetector.OnEmulatorDetectorListener() {
@@ -101,6 +101,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+        //MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 }
